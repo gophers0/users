899a42e17ba7db0f2beb8decb28a2493cbf2e563
Aleksei Loskutov <git@zergular.com>
2021-03-20 18:01:59 +0300

    Update make, docker

--- Makefile
+++ Makefile
@@ -1,19 +1,5 @@
-dev:
-	docker-compose -f docker-compose-dev.yml up -d
-dev-down:
-	docker-compose -f docker-compose-dev.yml down
-
 docker:
-	docker-compose up -d
+	docker-compose up -d && docker logs -f users_app_1
 
 docker-down:
 	docker-compose down
-
-build:
-	env GOOS=linux GOARCH=amd64 go build -o ./_build/users .
-
-deploy_dev: build
-	scp ./_build/marketplace s1:/srv/users/users_new
-	ssh -t market-dev 'systemctl stop users.service'
-	ssh -t market-dev 'rm /srv/users/current/users && rm /srv/users/users && mv /srv/users/users_new /srv/users/users && ln -s /srv/users/users /srv/users/current/users'
-	ssh -t market-dev 'systemctl start users.service'
--- docker/db/init-db.sql
+++ docker/db/init-db.sql
@@ -1,1 +1,23 @@
+create table users
+(
+    id         serial not null
+        constraint users_pkey
+            primary key,
+    created_at timestamp with time zone,
+    updated_at timestamp with time zone,
+    deleted_at timestamp with time zone,
+    login      text,
+    password   text,
+    role       text
+);
 
+alter table users
+    owner to app;
+
+create index idx_users_deleted_at
+    on users (deleted_at);
+
+create index idx_users_login
+    on users (login);
+
+INSERT INTO public.users (id, created_at, updated_at, deleted_at, login, password, role) VALUES (1, '2021-03-20 12:27:35.910000', '2021-03-20 12:27:37.016000', null, 'admin', '$2a$14$z.GRSWrKfsg6xsbtC4fMveXmTZKar53a.PJ1StRQjHmHn6qFf4.G.', 'admin');
