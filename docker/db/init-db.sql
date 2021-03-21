1b57635f7a608646d3330b20c6fce60621d5d28e
Aleksei Loskutov <git@zergular.com>
2021-03-21 08:22:31 +0300

    Fix init sql

--- docker/db/init-db.sql
+++ docker/db/init-db.sql
@@ -21,3 +21,5 @@ create index idx_users_login
     on users (login);
 
 INSERT INTO public.users (id, created_at, updated_at, deleted_at, login, password, role) VALUES (1, '2021-03-20 12:27:35.910000', '2021-03-20 12:27:37.016000', null, 'admin', '$2a$14$z.GRSWrKfsg6xsbtC4fMveXmTZKar53a.PJ1StRQjHmHn6qFf4.G.', 'admin');
+
+ALTER SEQUENCE users_id_seq RESTART WITH 2;
